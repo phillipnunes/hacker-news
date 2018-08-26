7aefd2ce197cd7203c6773d0355d9e1c470bdf89
Phillip Nunes <phillip_nunes@hotmail.com>
2018-08-26 12:56:33 -0300

    added react router in package.json

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import { Router, Route, Switch } from 'react-router';
 import logo from './logo.png'
 import './Header.css'
 
@@ -10,13 +11,13 @@ class Header extends Component {
           <img className="header__image" src={logo} alt="An amazing logo"/>
           <p className="header__title">Hacker News</p>
         </div>
-        <div className="header__menu">
-          <a href="#!" className="header__item-menu">news</a>
-          <span className="header__menu-divisor"></span>
-          <a href="#!" className="header__item-menu">best stories</a>
-          <span className="header__menu-divisor"></span>
-          <a href="#!" className="header__item-menu">donnuts?</a>
-        </div>
+          <div className="header__menu">
+            <a href="#!" className="header__item-menu">news</a>
+            <span className="header__menu-divisor"></span>
+            <a href="#!" className="header__item-menu">best stories</a>
+            <span className="header__menu-divisor"></span>
+            <a href="#!" className="header__item-menu">donnuts?</a>
+          </div>
       </header>
     )
   }
